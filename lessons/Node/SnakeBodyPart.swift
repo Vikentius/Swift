5a11ef7accb78d5958dc2737d1331ebf886dee64
VIkentius <vikentiuss@mail.ru>
2020-06-14 14:42:30 +0300

    Выполнила ДЗ 8

---
+++ lessons/Node/SnakeBodyPart.swift
@@ -0,0 +1,40 @@
+//
+//  SnakeBodyPart.swift
+//  lessons
+//
+//  Created by Shakhvorostov on 08.06.2020.
+//  Copyright © 2020 Shakhvorostov. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class SnakeBodyPart: SKShapeNode {
+    
+    let diametr = 10.0
+    
+    init(atPoint point: CGPoint) {
+        super.init()
+        
+        path = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: diametr, height: diametr)).cgPath
+        
+        fillColor = UIColor.green
+        strokeColor = UIColor.green
+        lineWidth = 5
+        
+        self.position = point
+        
+        self.physicsBody = SKPhysicsBody(circleOfRadius: CGFloat(diametr - 4), center: CGPoint(x:5, y: 5))
+        self.physicsBody?.isDynamic = true
+        
+        self.physicsBody?.categoryBitMask = CollisionCategories.Snake
+        
+        self.physicsBody?.contactTestBitMask = CollisionCategories.EdgeBody | CollisionCategories.Apple
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
